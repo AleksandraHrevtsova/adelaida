60a19f60d4affd116491efda2c767c8bb200bc38
Aleksandra Hrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2026-08-25 00:01:56 +0300

    updated custom links components

--- src/components/ui/Link.tsx
+++ src/components/ui/Link.tsx
@@ -1,27 +1,50 @@
-import Link from 'next/link';
+import { Link } from '@/i18n/navigation';
+import type { MouseEventHandler, ReactNode } from 'react';
 
 type Props = {
-  children?: React.ReactNode;
+  children?: ReactNode;
   label?: string;
   path: string;
+  onClick?: MouseEventHandler<HTMLAnchorElement>;
   isBlank?: boolean;
   className?: string;
 };
 
-export default function CustomLink({
+export const CustomLink = ({
   label, 
   path,
-  isBlank,
-  className,
+  onClick,
+  isBlank = false,
+  className = '',
   children,
-}: Props) {
+}: Props) => {
   return (
     <Link
       href={path}
-      target={isBlank ? '_blank' : '_self'}
+      target={isBlank ? '_blank' : undefined}
+      onClick={onClick}
       className={`${className} transition-opacity duration-300 hover:opacity-70`}
     >
       {label ?? children}
     </Link>
   );
-}
+};
+
+export const SocialLink = ({
+  label, 
+  path,
+  onClick,
+  isBlank = true,
+  className = '',
+}: Props) => {
+  return (
+    <Link
+      href={path}
+      target={isBlank ? '_blank' : undefined}
+      onClick={onClick}
+      className={`${className} transition-opacity duration-300 hover:opacity-70`}
+    >
+      {label}
+    </Link>
+  );
+};
