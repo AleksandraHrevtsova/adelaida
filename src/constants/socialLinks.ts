1684d2513ecf350ddece253b43fa4633e1612e52
Aleksandra Hrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2026-08-26 18:14:03 +0300

    updated linkedin link

--- src/constants/socialLinks.ts
+++ src/constants/socialLinks.ts
@@ -1,7 +1,7 @@
 const socialLinks = {
   instagram: 'https://www.instagram.com/adelaida_art_mask',
   facebook: 'https://www.facebook.com/adelaidaartmask',
-  linkedin: 'https://www.linkedin.com/me?trk=p_mwlite_feed-secondary_nav',
+  linkedin: 'https://www.linkedin.com/in/allaadelaidamask/',
   akcentra_linkedin: 'https://www.linkedin.com/company/110310623'
 };
 
